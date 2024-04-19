3c3501567ef40eeeec8e216c3ba8ca37040b7166
0bscenity <154705783+0bscenity@users.noreply.github.com>
2024-04-19 09:58:50 -0500

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,23 @@
+sudo dnf update -y && sudo dnf upgrade -y
+sudo dnf install vlc -y
+sudo dnf install neofetch -y
+sudo dnf install screenfetch -y
+sudo dnf install snapd -y
+sudo systemctl enable --now snapd
+sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+sudo flatpak install flathub io.github.peazip.PeaZip -y
+sudo flatpak install flathub com.visualstudio.code -y
+sudo flatpak install flathub com.spotify.Client -y
+sudo flatpak install flathub org.qbittorrent.qBittorrent -y
+sudo flatpak install flathub org.freedownloadmanager.Manager -y
+sudo flatpak install flathub com.gitlab.davem.ClamTk -y
+sudo flatpak install flathub org.onlyoffice.desktopeditors -y
+sudo flatpak install flathub org.gnome.TextEditor -y
+sudo flatpak install flathub io.github.jonmagon.kdiskmark -y
+sudo snap install easy-disk-cleaner -y
+sudo flatpak install flathub com.github.jkotra.eovpn -y
+sudo snap install riseup-vpn --classic -y
+sudo snap install snap-store -y
+neofetch
+sleep 2 #
+sudo reboot
