daa901d07f2bfeda5bb0788401566f2596376c21
0bscenity <154705783+0bscenity@users.noreply.github.com>
2024-04-19 10:01:05 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,8 @@ sudo dnf update -y && sudo dnf upgrade -y
 sudo dnf install vlc -y
 sudo dnf install neofetch -y
 sudo dnf install screenfetch -y
-sudo dnf install snapd -y
+
+sudo dnf install snapd
 sudo systemctl enable --now snapd
 sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 sudo flatpak install flathub io.github.peazip.PeaZip -y
@@ -14,7 +15,7 @@ sudo flatpak install flathub com.gitlab.davem.ClamTk -y
 sudo flatpak install flathub org.onlyoffice.desktopeditors -y
 sudo flatpak install flathub org.gnome.TextEditor -y
 sudo flatpak install flathub io.github.jonmagon.kdiskmark -y
-sudo snap install easy-disk-cleaner -y
+sudo snap install easy-disk-cleaner
 sudo flatpak install flathub com.github.jkotra.eovpn -y
 sudo snap install riseup-vpn --classic -y
 sudo snap install snap-store -y
