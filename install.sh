3148d05dc1ac8055e6b202f133e0301e8431246e
0bscenity <154705783+0bscenity@users.noreply.github.com>
2024-04-24 19:50:12 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,23 +1,16 @@
 sudo dnf update -y && sudo dnf upgrade -y
 sudo dnf remove firefox -y
-sudo dnf install vlc neofetch screenfetch snapd ffmpeg mpv tor -y
-sudo systemctl enable --now snapd
+sudo dnf install vlc neofetch screenfetch ffmpeg mpv tor -y
 sudo flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
-sudo flatpak install flathub io.github.peazip.PeaZip -y
-sudo flatpak install flathub com.visualstudio.code -y
-sudo flatpak install flathub com.spotify.Client -y
-sudo flatpak install flathub org.qbittorrent.qBittorrent -y
+sudo flatpak install flathub io.github.peazip.PeaZip
+sudo flatpak install flathub com.visualstudio.code
+sudo flatpak install flathub com.spotify.Client
 sudo dnf install google-noto-sans* --exclude=google-noto-sans-cjk*,google-noto-sans-hk*
-sudo flatpak install flathub org.freedownloadmanager.Manager -y
-sudo flatpak install flathub com.gitlab.davem.ClamTk -y
-sudo flatpak install flathub org.onlyoffice.desktopeditors -y
-sudo flatpak install flathub io.github.jonmagon.kdiskmark -y
-sudo snap install easy-disk-cleaner
-sudo flatpak install flathub com.github.jkotra.eovpn -y
-sudo snap install riseup-vpn --classic
-sudo snap install snap-store
-flatpak install flathub org.mozilla.firefox -y
-flatpak install flathub io.github.f3d_app.f3d -y
+sudo flatpak install flathub org.freedownloadmanager.Manager
+sudo flatpak install flathub com.gitlab.davem.ClamTk
+sudo flatpak install flathub org.onlyoffice.desktopeditors
+flatpak install flathub org.mozilla.firefox
+flatpak install flathub io.github.f3d_app.f3d
 
 neofetch
 
